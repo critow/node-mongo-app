72e9da92be1a3d27779b402453ee3a2c5f8e392f
Andrew Kurmaev <critowpas@mail.ru>
2020-05-05 16:40:39 +0300

    Created app

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const exphbs = require('express-handlebars')
+const todoRoutes = require('./routes/todos')
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.engine('hbs', exphbs({
+    defaultLayout: 'main',
+    extname: 'hbs',
+    helpers: {
+        todo: obj => obj.title,
+        id: obj => obj._id,
+        completed: obj => obj.completed
+    }
+}))
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(todoRoutes)
+
+async function start() {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/todos', {
+            useNewUrlParser: true,
+            useFindAndModify: false
+        })
+        app.listen(PORT, () => console.log(`Server has been started on port: ${PORT}`))
+    } catch (e) {
+        console.log(e)
+
+    }
+}
+
+start()
